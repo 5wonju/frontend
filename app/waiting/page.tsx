8bef9841f46c0fbfd59d2e0aa051b4c91572e95b
leewooseong <sitpo96@naver.com>
2024-04-22 03:02:21 +0900

    Feat #2: 대기방 리스트 UI

---
+++ app/waiting/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import WaitingRoomList from '../waiting/ui/WaitingRoomList'
+
+const Page = () => {
+	return <WaitingRoomList />
+}
+
+export default Page
---
+++ app/waiting/ui/WaitingRoomList.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import WaitingRoom from './WaitingRoom'
+import { generateRooms } from '../lib/util'
+
+const RoomListComponent: React.FC = () => {
+	const [rooms, setRooms] = React.useState<Room[]>([])
+	useEffect(() => {
+		setRooms(generateRooms())
+	}, [])
+	useEffect(() => {
+		console.log('rooms', rooms)
+	}, [rooms])
+
+	// const roomsData = generateRooms()
+
+	return (
+		<div className="grid grid-cols-2 gap-4">
+			{rooms.map((room) => (
+				<WaitingRoom key={room.roomId} room={room} />
+			))}
+		</div>
+	)
+}
+
+export default RoomListComponent
