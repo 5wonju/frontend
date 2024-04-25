0de6b817e427966199cbc7e02730d9a6b470e2b7
이우성 <sitpo96@naver.com>
2024-04-25 15:23:04 +0900

    Feat #3: WebSocket API를 사용하여 소켓 통신 구현하도록 수정
    - 서버에서 어떤 방식으로 Socket 통신을 구현할지 모르기 때문에 WebSocket API를 사용하여 클라이언트 소켓 통신 구현하도록 수정

---
+++ frontend/app/lib/hooks/socket.ts
@@ -0,0 +1,86 @@
+import { getSocketToken } from '../api'
+import { useChatSocketStore, useGameSocketStore } from './../store'
+
+// Todo: 이 코드는 작성 시(or 완료) util이나 services로 빼서 작성
+// Todo : 게임 진행 중에 필요한 동작을 정의
+// gameSocket에 필요한 설정을 수행하는 함수
+export const setUpGameSocket = (socket: WebSocket) => {
+	// 예시
+	// socket.onmessage = (event) => {
+	// 	console.log('Message from server:', event.data)
+	// }
+}
+
+// 소캣을 수행하는 함수를 리턴하는 커스텀 훅
+export const useConnectSocket = (type: 'game' | 'chat') => {
+	const {
+		gameSocket,
+		setGameSocket,
+		removeGameSocket,
+		reconnectAttempts: gameReconnectAttempts,
+		maxReconnectAttempts: gameMaxReconnectAttempts,
+		setConnectAttempts: setGameConnectAttempts,
+	} = useGameSocketStore()
+
+	const {
+		chatSocket,
+		setChatSocket,
+		removeChatSocket,
+		reconnectAttempts: chatReconnectAttempts,
+		maxReconnectAttempts: chatMaxReconnectAttempts,
+		setConnectAttempts: setChatConnectAttempts,
+	} = useChatSocketStore()
+
+	// 사용할 소켓 정보 설정
+	const socket = type === 'game' ? gameSocket : chatSocket
+	const setSocket = type === 'game' ? setGameSocket : setChatSocket
+	const removeSocket = type === 'game' ? removeGameSocket : removeChatSocket
+	const reconnectAttempts = type === 'game' ? gameReconnectAttempts : chatReconnectAttempts
+	const maxReconnectAttempts = type === 'game' ? gameMaxReconnectAttempts : chatMaxReconnectAttempts
+	const setConnectAttempts = type === 'game' ? setGameConnectAttempts : setChatConnectAttempts
+
+	const connectSocket = async () => {
+		const socketToken = await getSocketToken()
+
+		// WebSocket 생성 및 연결
+		const url = `wss://localhost:3000/socket`
+		const newSocket = new WebSocket(url, [socketToken])
+
+		// 연결 성공
+		newSocket.onopen = () => {
+			console.log('WebSocket connection established.')
+			socket?.close() // 기존 소켓이 있으면 닫음
+			setConnectAttempts(0) // 연결 성공 시 재연결 시도 횟수 초기화
+			setSocket(socket)
+		}
+
+		// 연결 종료
+		newSocket.onclose = (event) => {
+			// 의도치 않은 연결 종료 시 재연결 시도
+			if (!event.wasClean && reconnectAttempts < maxReconnectAttempts) {
+				console.log(`Connection lost, attempting to reconnect... (${reconnectAttempts + 1})`)
+				// 재시도 횟수에 비례하여 지연 시간을 늘림 + 재시도
+				let delay = Math.pow(2, reconnectAttempts) * 1000 // 지수 백오프 계산
+				setTimeout(() => {
+					if (reconnectAttempts < maxReconnectAttempts) {
+						setConnectAttempts(reconnectAttempts + 1) // 연결 성공 시 재연결 시도 횟수 초기화
+						connectSocket()
+					}
+				}, delay)
+			}
+			// 연결이 깨끗하게 종료되었거나 최대 재연결 횟수에 도달했을 때
+			else {
+				console.log('WebSocket closed: ', event.reason)
+				socket?.close() // 소켓이 있으면 닫음
+				removeSocket()
+			}
+		}
+
+		// 에러 발생
+		newSocket.onerror = (error) => {
+			console.log('WebSocket error:', error)
+		}
+	}
+
+	return { useConnectSocket }
+}
--- frontend/app/lib/store.ts
+++ frontend/app/lib/store.ts
@@ -1,27 +1,72 @@
-import { Socket, io } from 'socket.io-client'
-import create, { StateCreator, StoreApi } from 'zustand'
+import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 
 interface GameSocket {
-	gameSocket: Socket | null
-	setGameSocket: (socket: Socket) => void
+	gameSocket: WebSocket | null
+	setGameSocket: (socket: WebSocket | null) => void
 	removeGameSocket: () => void
+	reconnectAttempts: number // 현재 재연결 시도 횟수
+	maxReconnectAttempts: number // 최대 재연결 시도 횟수
+	setConnectAttempts: (attempts: number) => void
 }
 
 export const useGameSocketStore = create<GameSocket>()(
 	devtools(
-		(set) => ({
+		(set, get) => ({
 			gameSocket: null, // 초기 소켓 상태는 null
-			setGameSocket: (socket: Socket) => {
-				set({ gameSocket: socket })
+			reconnectAttempts: 0,
+			maxReconnectAttempts: 5,
+			setGameSocket: (socket: WebSocket | null) => {
+				set((state) => {
+					state.gameSocket?.close() // 기존 소켓이 있으면 닫고 새로운 소켓으로 교체
+					return { gameSocket: socket }
+				})
 			},
 			removeGameSocket: () => {
 				set((state) => {
+					state.gameSocket?.close() // 소켓이 있으면 닫음
 					return { gameSocket: null }
 				})
+			},
+			setConnectAttempts: (attempts: number) => {
+				set({ reconnectAttempts: attempts })
 			},
 		}),
 		{ name: 'GameSocketStore' } // Redux DevTools에서 볼 수 있는 스토어의 이름
 	)
 )
-export default useGameSocketStore
+
+interface ChatSocket {
+	chatSocket: WebSocket | null
+	setChatSocket: (socket: WebSocket | null) => void
+	removeChatSocket: () => void
+	reconnectAttempts: number // 현재 재연결 시도 횟수
+	maxReconnectAttempts: number // 최대 재연결 시도 횟수
+	setConnectAttempts: (attempts: number) => void
+}
+
+export const useChatSocketStore = create<ChatSocket>()(
+	devtools(
+		(set, get) => ({
+			chatSocket: null, // 초기 소켓 상태는 null
+			reconnectAttempts: 0,
+			maxReconnectAttempts: 5,
+			setChatSocket: (socket: WebSocket | null) => {
+				set((state) => {
+					state.chatSocket?.close() // 기존 소켓이 있으면 닫고 새로운 소켓으로 교체
+					return { chatSocket: socket }
+				})
+			},
+			removeChatSocket: () => {
+				set((state) => {
+					state.chatSocket?.close() // 소켓이 있으면 닫음
+					return { chatSocket: null }
+				})
+			},
+			setConnectAttempts: (attempts: number) => {
+				set({ reconnectAttempts: attempts })
+			},
+		}),
+		{ name: 'ChatSocketStore' } // Redux DevTools에서 볼 수 있는 스토어의 이름
+	)
+)
