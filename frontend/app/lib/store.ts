c4046f7afbe4e10670d115c6e8fcd4e87a68359b
이우성 <sitpo96@naver.com>
2024-04-25 09:58:49 +0900

    Refact #3: store에 코드에 저장 로직만 담도록 코드 리팩토링

--- frontend/app/lib/store.ts
+++ frontend/app/lib/store.ts
@@ -4,42 +4,19 @@ import { devtools } from 'zustand/middleware'
 
 interface GameSocket {
 	gameSocket: Socket | null
-	connectGameSocket: (socketToken: string) => void
-	disconnectGameSocket: () => void
+	setGameSocket: (socket: Socket) => void
+	removeGameSocket: () => void
 }
 
 export const useGameSocketStore = create<GameSocket>()(
 	devtools(
 		(set) => ({
 			gameSocket: null, // 초기 소켓 상태는 null
-			connectGameSocket: (socketToken: string) => {
-				const socket = io('http://your-server.com', {
-					autoConnect: false,
-					reconnectionAttempts: 5, // 최대 재연결 시도 횟수
-					reconnectionDelay: 3000, // 재연결 시도 간격
-					auth: {
-						token: socketToken, // 인증을 위한 임시 토큰을 포함하여 소켓 연결
-					},
-				})
-
-				socket.connect() // autoConnect가 false로 설정되어 있으므로 수동으로 연결
-
-				socket.on('connect', () => {
-					console.log('소켓 연결됨:', socket.id)
-					set({ gameSocket: socket })
-				})
-
-				socket.on('disconnect', (reason) => {
-					if (reason === 'io server disconnect') {
-						socket.connect()
-					}
-					console.log('소켓 연결 끊김:', reason)
-					set({ gameSocket: null })
-				})
+			setGameSocket: (socket: Socket) => {
+				set({ gameSocket: socket })
 			},
-			disconnectGameSocket: () => {
+			removeGameSocket: () => {
 				set((state) => {
-					state.gameSocket?.disconnect()
 					return { gameSocket: null }
 				})
 			},
--- frontend/app/lib/util.ts
+++ frontend/app/lib/util.ts
@@ -1,7 +1,46 @@
+import { Socket, io } from 'socket.io-client'
 import { getSocketToken } from './api'
+import useGameSocketStore from './store'
+
+export const createSocket = (socketToken: string) => {
+	const socket = io('http://your-server.com', {
+		autoConnect: false,
+		reconnectionAttempts: 5, // 최대 재연결 시도 횟수
+		reconnectionDelay: 3000, // 재연결 시도 간격
+		auth: {
+			token: socketToken, // 인증을 위한 임시 토큰을 포함하여 소켓 연결
+		},
+	})
+
+	return socket
+}
 
 // 중요 로직과 관련 없는 유틸리티 함수들을 관리하는 파일
-export const connectSocketWithToken = async (connectSocketFn: (socketToken: string) => void) => {
+export const connectSocketWithToken = async () => {
 	const socketToken = await getSocketToken()
-	connectSocketFn(socketToken)
+	const newSocket = createSocket(socketToken)
+	newSocket.connect() // autoConnect가 false로 설정되어 있으므로 수동으로 연결
+}
+
+export const disconnectSocket = (connectedSocket: Socket, removeSocket: () => void) => {
+	connectedSocket?.disconnect()
+	removeSocket()
+}
+
+// 생성된 소켓에 대한 초기 설정을 하는 함수
+export const useSocketInitialSetting = (socket: Socket) => {
+	const { setGameSocket, removeGameSocket } = useGameSocketStore()
+
+	socket.on('connect', () => {
+		console.log('소켓 연결됨:', socket.id)
+		setGameSocket(socket)
+	})
+
+	socket.on('disconnect', (reason) => {
+		if (reason === 'io server disconnect') {
+			socket.connect()
+		}
+		console.log('소켓 연결 끊김:', reason)
+		removeGameSocket()
+	})
 }
