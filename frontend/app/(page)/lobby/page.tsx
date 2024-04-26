d821e11c3966c60cfca24d23437e1c704759c7ff
이우성 <sitpo96@naver.com>
2024-04-26 14:14:21 +0900

    Feat #6: 회원 정보 불러오도록 임시 api 환경 셋팅

--- frontend/app/(page)/lobby/page.tsx
+++ frontend/app/(page)/lobby/page.tsx
@@ -1,8 +1,19 @@
 'use client'
 
+import { useAuth } from '@/app/hooks/useAuth'
 import WaitingRoomList from './component/WaitingRoomList'
+import { useEffect } from 'react'
 
 const Lobby = () => {
+	const { userInfo, refetch } = useAuth()
+
+	useEffect(() => {
+		refetch()
+	}, [])
+	useEffect(() => {
+		console.log(userInfo)
+	}, [userInfo])
+
 	return <WaitingRoomList />
 }
 
