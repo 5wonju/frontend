68bff3287e2c12c1041850a627bb1947ad317e4a
Chosamee <cytor@naver.com>
2024-05-03 15:58:09 +0900

    Feat #31: Add 채팅 buffer

--- frontend/app/(page)/channel/lib/store.ts
+++ frontend/app/(page)/channel/lib/store.ts
@@ -1,6 +1,11 @@
 import { create } from 'zustand'
 
-interface SocketState {
+interface IChat {
+  sender: string
+  message: string
+}
+
+interface ISocketState {
   socket: WebSocket | null
   isConnected: boolean
   region: string | null
@@ -10,23 +15,27 @@ interface SocketState {
   setRegion: (region: string | null) => void
   connect: (region: string, socketToken: string) => void
   disconnect: () => void
-  sendMessage: (message: string) => void
+  sendMessage: (eventType: string, message: string) => void
   messages: string[] // 메시지 배열
   addMessage: (message: string) => void // 메시지 추가 함수
+  chatLogs: string[] // 채팅 로그 배열
+  addChatLogs: (message: string) => void // 채팅 로그 추가 함수
 }
 
-export const useSocketStore = create<SocketState>((set, get) => ({
+export const useSocketStore = create<ISocketState>((set, get) => ({
   socket: null,
   isConnected: false,
   region: null,
   reconnectAttempts: 0,
   maxReconnectAttempts: 5, // 최대 재연결 시도 횟수
   messages: [],
+  chatLogs: [],
   addMessage: (message) => {
     set((state) => ({ messages: [...state.messages, message] }))
   },
-  sendMessage: (message: string) => {
-    get().socket?.send(message)
+  sendMessage: (eventType: string, message: string) => {
+    console.log('Sending message:', message)
+    get().socket?.send(JSON.stringify({ eventType: eventType, data: { message: message } }))
   },
   setSocket: (socket) => {
     set({ socket })
@@ -38,6 +47,7 @@ export const useSocketStore = create<SocketState>((set, get) => ({
     // const ws = new WebSocket(`wss://localhost:3000/ws/${region}?Authorization=${socketToken}`)
     // const ws = new WebSocket(`wss://localhost:8080/ws?Authorization=${socketToken}`)
     const ws = new WebSocket(`wss://dummy.mo-or-do.net/ws?Authorization=${socketToken}`)
+    // const ws = new WebSocket(`ws://localhost:8080/ws`)
 
     ws.onopen = () => {
       console.log('Connected to WebSocket')
@@ -58,9 +68,20 @@ export const useSocketStore = create<SocketState>((set, get) => ({
     }
     ws.onmessage = (event) => {
       console.log('Received message:', event.data)
-      get().addMessage(event.data)
+      // TODO: event.data를 파싱하여 채팅 로그에 추가하는 로직 필요
+      get().addChatLogs(event.data)
+      // get().addMessage(event.data)
     }
     set({ socket: ws, region })
+  },
+  addChatLogs: (message) => {
+    // TODO: sender 받아서 IChat으로 바꿀 것
+    set((state) => ({ chatLogs: [...state.chatLogs, message] }))
+    const maxLenth = 3
+    if (get().chatLogs.length > maxLenth) {
+      set((state) => ({ chatLogs: state.chatLogs.slice(-1 * maxLenth) }))
+    }
+    console.log(get().chatLogs)
   },
   disconnect: () => {
     get().socket?.close()
--- frontend/app/component/Chat.tsx
+++ frontend/app/component/Chat.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import { useSocketStore } from '../(page)/channel/lib/store'
 
 const Chat = () => {
-  const { sendMessage } = useSocketStore()
+  const { sendMessage, chatLogs } = useSocketStore()
+  const [message, setMessage] = useState('')
   return (
     <>
       <div>채팅 컴포넌트</div>
-      <input type="text" />
-      <button onClick={() => sendMessage('test')}>전송</button>
+      <div>
+        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button
+          onClick={() => {
+            if (message.trim() === '') return
+            sendMessage('CHATTING', message)
+            setMessage('')
+          }}
+        >
+          전송
+        </button>
+      </div>
+      <div>
+        {chatLogs.map((log, index) => (
+          <div key={index}>{log}</div>
+        ))}
+      </div>
     </>
   )
 }
