aff0bb6a3f6f555dbb06b09e98b3ff51817bbdd9
이원주 <dldnjswn4265@gmail.com>
2024-05-07 14:53:53 +0900

    Feat #41: 정답 필드 순서 변경

--- frontend/app/(page)/game/component/AnswerSpot.tsx
+++ frontend/app/(page)/game/component/AnswerSpot.tsx
@@ -3,9 +3,9 @@ import { CylinderCollider, RigidBody } from '@react-three/rapier'
 import React from 'react'
 
 const AnswerSpot = () => {
-  const answers = ['A', 'B', 'C', 'D']
+  const answers = ['A', 'C', 'D', 'B']
   return answers.map((answer, index) => (
-    <group key={answer} rotation-y={(index / 4) * Math.PI * 2}>
+    <group key={answer} rotation-y={((index + 1) / 4) * Math.PI * 2}>
       <group position-x={8} position-z={-8}>
         <RigidBody colliders={false} type="fixed">
           <CylinderCollider args={[2 / 2, 3]} />
@@ -18,7 +18,7 @@ const AnswerSpot = () => {
           <Text3D
             font={'./fonts/Geologica Thin_Regular.json'}
             size={2}
-            rotation-y={-(index / 4) * Math.PI * 2}
+            rotation-y={((index + 1) / 4) * Math.PI * 2 * (index % 2 === 0 ? -1 : 1)}
             bevelEnabled={true}
             bevelThickness={0.3}
             bevelSize={0.05}
