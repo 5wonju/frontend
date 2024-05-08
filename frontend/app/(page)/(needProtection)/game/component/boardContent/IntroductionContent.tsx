91bff9ebec0182abdcfe5825e37fc52456ee6a00
이원주 <dldnjswn4265@gmail.com>
2024-05-08 23:04:56 +0900

    Refact #53: 전광판 방 정보 컴포넌트 분리

--- frontend/app/(page)/(needProtection)/game/component/boardContent/IntroductionContent.tsx
+++ frontend/app/(page)/(needProtection)/game/component/boardContent/IntroductionContent.tsx
@@ -1,18 +1,11 @@
+import RoomInfo from "./RoomInfo"
+
+
 const IntroductionContent = () => {
+  const roomId = 2
   return (
     <div className="select-none">
-      <h1 className="glass flex justify-between text-black px-5">
-        <fieldset className="flex gap-3">
-          <span>001</span>
-          <span>원주의방</span>
-        </fieldset>
-        <fieldset className="flex gap-3">
-          <span>개발</span>
-          <span>8 Round</span>
-          {/* 게임 시작 후에 보일 부분 */}
-          <span>2/8</span>
-        </fieldset>
-      </h1>
+      <RoomInfo roomId={roomId} />
     </div>
   )
 }
