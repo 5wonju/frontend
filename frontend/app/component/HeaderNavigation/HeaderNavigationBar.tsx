4200ff781d23f3a0de6a10ad6a58f2f2efa664c7
이원주 <dldnjswn4265@gmail.com>
2024-05-08 17:17:07 +0900

    Design: 헤더 높이 수정

--- frontend/app/component/HeaderNavigation/HeaderNavigationBar.tsx
+++ frontend/app/component/HeaderNavigation/HeaderNavigationBar.tsx
@@ -4,7 +4,7 @@ import QuickStart from './QuickStart'
 
 const HeaderNavigationBar = () => {
   return (
-    <section className="flex justify-between items-center absolute mt-2 bg-white top-0 h-12 w-full px-4 select-none">
+    <section className="h-14 flex justify-between items-center absolute bg-white top-0 w-full px-4 select-none">
       <fieldset>
         <div className="text-purple-300 font-bold my-auto text-center text-2xl px-3">LOGO</div>
       </fieldset>
