0d5f8d42b5d1e5b817df1033b69d51a66604e7ca
이원주 <dldnjswn4265@gmail.com>
2024-05-08 15:41:22 +0900

    Feat #41: 전광판 퀴즈 정답 순위 보드 UI 수정 및 애니메이션 추가

--- frontend/app/(page)/game/component/gameInfoPanel/QuizResult.tsx
+++ frontend/app/(page)/game/component/gameInfoPanel/QuizResult.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { teamEnum } from '../../lib/store'
 import { teamColorToCss } from './TeamInfo'
 
@@ -15,6 +16,7 @@ const rankList: { [key: number]: string } = {
   3: '3rd',
 }
 
+// 더미 데이터
 const wonUserList: IWonUser[] = [
   {
     nickname: 'user1s-3we',
@@ -41,31 +43,65 @@ const wonUserList: IWonUser[] = [
 
 const QuizResult = () => {
   const quizNumber = 1
+  const [result, setResult] = useState({
+    winTeam: false,
+    0: false,
+    1: false,
+    2: false,
+  })
+  useEffect(() => {
+    const keys = ['2', '1', '0', 'winTeam']
+    keys.forEach((key, index) => {
+      setTimeout(() => {
+        setResult((prevState) => ({
+          ...prevState,
+          [key]: true,
+        }))
+      }, 500 * (index + 1))
+    })
+
+    return () =>
+      keys.forEach((key, index) => {
+        clearTimeout(500 * (index + 1))
+      })
+  }, [])
+
   return (
-    <div className="board flex flex-col items-center p-4">
-      <h1 className="text-lg text-lightGray1 font-semibold pb-2">Quiz. {quizNumber}</h1>
-      <section className="flex items-center gap-2 font-bold text-3xl animate-floating">
-        <span className={`text-red-400`}>RED</span>
-        <span className="flex items-center gap-1 text-yellow-400">
-          WIN
+    <div className="min-h-full flex flex-col items-center py-4 animate-floating">
+      {/* 우승팀 결과 or 기본 정보 */}
+      <section
+        className={`glass px-2 py-3 ${
+          result.winTeam ? 'visible animate-fadeInLast duration-500' : 'invisible'
+        }`}
+      >
+        <div className="flex items-center gap-2 font-bold text-3xl">
+          <span className={`text-red-400`}>RED</span>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Star.png"
             alt="Star"
-            className="size-10"
+            className=" size-6"
           />
-        </span>
+          <span className="flex items-center gap-1 text-yellow-400">WIN</span>
+        </div>
       </section>
-      <ul className="flex-1 flex flex-col justify-around">
+      {/* 리스트 요소 하나씩 등장하기 */}
+      <ul className="flex-1 flex flex-col gap-3 pt-5 justify-around">
         {wonUserList.map((wonUser, index) => (
-          <li key={index} className="flex gap-4 justify-between items-end px-2">
-            <p className="font-bold text-2xl text-darkGray3">{rankList[wonUser.rank]}.</p>
+          <li
+            key={index}
+            className={`${
+              result[index as keyof typeof result] ? 'visible animate-fadeIn' : 'invisible'
+            }
+            glass flex gap-4 justify-around items-end px-3 py-2`}
+          >
+            <p className="font-semibold text-xl text-darkGray3">{rankList[wonUser.rank]}.</p>
             <p className="fle1 flex flex-col items-start">
-              <span className="font-light text-sm text-lightGray1">{wonUser.time}초</span>
+              <span className="font-normal text-xs text-darkGray3 h-3">{wonUser.time}초</span>
               <span
                 className={`${
                   teamColorToCss[wonUser.team]
-                } font-semibold text-2xl overflow-hidden w-24 select-none truncate`}
+                } font-medium text-xl overflow-hidden w-24 select-none truncate`}
               >
                 {wonUser.nickname}
               </span>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,100 +2,112 @@ import { transcode } from 'buffer'
 import type { Config } from 'tailwindcss'
 
 const colorPalette = {
-	// Mono Colors
-	black: '#454645',
-	darkGray1: '#767676',
-	darkGray2: '#8D8787',
-	darkGray3: '#9BA2AE',
-	darkGray4: '#C0B9B9',
-	lightGray1: '#C4C4C4',
-	lightGray2: '#D9D9D9',
-	lightGray3: '#E5E6E9',
-	lightGray4: '#F3F3F3',
-	lightGray5: '#FAFAFA',
-	white: '#ffffff',
+  // Mono Colors
+  black: '#454645',
+  darkGray1: '#767676',
+  darkGray2: '#8D8787',
+  darkGray3: '#9BA2AE',
+  darkGray4: '#C0B9B9',
+  lightGray1: '#C4C4C4',
+  lightGray2: '#D9D9D9',
+  lightGray3: '#E5E6E9',
+  lightGray4: '#F3F3F3',
+  lightGray5: '#FAFAFA',
+  white: '#ffffff',
 
-	// Main Colors
+  // Main Colors
 }
 
 const config: Config = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			colors: colorPalette,
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
-			keyframes: {
-				floating: {
-					'0%, 100%': {
-						transform: 'translateY(0px)',
-					},
-					'50%': {
-						transform: 'translateY(-5px)',
-					},
-				},
-				faceInFromButtom1: {
-					'0%': {
-						transform: 'translateY(10px)',
-						opacity: '0%',
-					},
-					'20%': {
-						transform: 'translateY(0)',
-						opacity: '100%',
-					},
-					'70%': {
-						transform: 'translateY(0)',
-						opacity: '100%',
-					},
-					'90%': {
-						transform: 'translateY(-15px)',
-						opacity: '10%',
-					},
-					'100%': {
-						transform: 'translateY(-15px)',
-						opacity: '0%',
-					},
-				},
-				faceInFromButtom2: {
-					'0%': {
-						opacity: '0%',
-					},
-					'10%': {
-						transform: 'translateY(10px)',
-						opacity: '0%',
-					},
-					'30%': {
-						transform: 'translateY(0)',
-						opacity: '100%',
-					},
-					'80%': {
-						transform: 'translateY(0)',
-						opacity: '100%',
-					},
-					'90%': {
-						transform: 'translateY(-15px)',
-						opacity: '10%',
-					},
-					'100%': {
-						transform: 'translateY(-15px)',
-						opacity: '0%',
-					},
-				},
-			},
-		},
-		animation: {
-			floating: '1.8s infinite ease-in-out floating',
-			// 애니메이션이 끝나는 시점보다 문구가 바뀌는 시점이 빠르도록 500ms를 더 줌 (setInterval의 부정확성 예방)
-			faceInFromButtom1: '4.5s infinite ease-in-out faceInFromButtom1',
-			faceInFromButtom2: '4.5s infinite ease-in-out faceInFromButtom2',
-		},
-	},
-	plugins: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: colorPalette,
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        fadeIn: {
+          from: {
+            opacity: '0',
+            transform: 'translateX(20px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        floating: {
+          '0%, 100%': {
+            transform: 'translateY(0px)',
+          },
+          '50%': {
+            transform: 'translateY(-5px)',
+          },
+        },
+        faceInFromButtom1: {
+          '0%': {
+            transform: 'translateY(10px)',
+            opacity: '0%',
+          },
+          '20%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+          },
+          '70%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+          },
+          '90%': {
+            transform: 'translateY(-15px)',
+            opacity: '10%',
+          },
+          '100%': {
+            transform: 'translateY(-15px)',
+            opacity: '0%',
+          },
+        },
+        faceInFromButtom2: {
+          '0%': {
+            opacity: '0%',
+          },
+          '10%': {
+            transform: 'translateY(10px)',
+            opacity: '0%',
+          },
+          '30%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+          },
+          '80%': {
+            transform: 'translateY(0)',
+            opacity: '100%',
+          },
+          '90%': {
+            transform: 'translateY(-15px)',
+            opacity: '10%',
+          },
+          '100%': {
+            transform: 'translateY(-15px)',
+            opacity: '0%',
+          },
+        },
+      },
+    },
+    animation: {
+      floating: '1.8s infinite ease-in-out floating',
+      // 애니메이션이 끝나는 시점보다 문구가 바뀌는 시점이 빠르도록 500ms를 더 줌 (setInterval의 부정확성 예방)
+      faceInFromButtom1: '4.5s infinite ease-in-out faceInFromButtom1',
+      faceInFromButtom2: '4.5s infinite ease-in-out faceInFromButtom2',
+      fadeIn: 'fadeIn 0.3s ease-in-out forwards',
+      fadeInLast: 'fadeIn 0.7s ease-in-out forwards',
+    },
+  },
+  plugins: [],
 }
 export default config
