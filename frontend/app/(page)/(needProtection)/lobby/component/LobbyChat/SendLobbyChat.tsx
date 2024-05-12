1c0d71a0d26c6d337b51092af50dbe77dfc878af
leewooseong <sitpo96@naver.com>
2024-05-13 07:44:01 +0900

    Feat #70: 글자 수 제한 로직 추가

--- frontend/app/(page)/(needProtection)/lobby/component/LobbyChat/SendLobbyChat.tsx
+++ frontend/app/(page)/(needProtection)/lobby/component/LobbyChat/SendLobbyChat.tsx
@@ -4,6 +4,8 @@ import { useChat } from '@/app/hooks/useSocket'
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useMemo, useState } from 'react'
 
+const MAX_CHAT_LEN = 50
+
 const SendLobbyChat = () => {
   // const { socket } = useSocketStore()
   const { sendChat } = useChat()
@@ -11,28 +13,42 @@ const SendLobbyChat = () => {
   const searchParams = useSearchParams()
   const regionInfo = searchParams.get('region') ?? '게임 전체'
 
-  // Todo: 최대 200자 제한 로직 추가
+  useEffect(() => {
+    console.log(message, 'in useEffect')
+  }, [message])
+
+  const messageLengthValidation = (message: string): boolean => {
+    if (message.length > MAX_CHAT_LEN) {
+      alert(`메세지는 ${MAX_CHAT_LEN}자 이하로 입력해주세요.`)
+      return false
+    }
+    return true
+  }
+
+  // Todo: 최대 50자 제한 로직 추가
   // Todo: 수신 메세지 최대 100개로 수정
   return (
     <div className="flex w-full z-10 bg-black rounded  gap-2">
       <p className="bg-indigo-500 py-1 px-4  text-white">{regionInfo}</p>
       <input
         type="text"
-        placeholder="메세지를 입력해주세요. (최대 200자)"
+        placeholder={`메세지를 입력해주세요. (최대 ${MAX_CHAT_LEN}자)`}
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            if (message.trim() === '') return
-            sendChat(message)
-            setMessage('')
-          }
+          if (e.key !== 'Enter' || !messageLengthValidation(message) || message.trim() === '')
+            return
+
+          console.log('키 다운 이벤트 발생:', e.key)
+          sendChat(message)
+          setMessage('')
         }}
         className="grow bg-none placeholder:text-white text-white focus:outline-none bg-transparent px-2 py-1"
       />
       <button
         onClick={() => {
-          if (message.trim() === '') return
+          if (!messageLengthValidation(message) || message.trim() === '') return
+
           sendChat(message)
           setMessage('')
         }}
