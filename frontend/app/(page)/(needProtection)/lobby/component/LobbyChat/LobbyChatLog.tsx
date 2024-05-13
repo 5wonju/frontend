f7cb6d04896ccfeacd2b9112feb4ffbf01b0f381
이우성 <sitpo96@naver.com>
2024-05-13 09:24:27 +0900

    Fix #70: 채팅 넘어갈 경우 채팅 맨 아래로 이동하도록 수정

--- frontend/app/(page)/(needProtection)/lobby/component/LobbyChat/LobbyChatLog.tsx
+++ frontend/app/(page)/(needProtection)/lobby/component/LobbyChat/LobbyChatLog.tsx
@@ -1,17 +1,31 @@
 import { useChat } from '@/app/hooks/useSocket'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const LobbyChatLog = () => {
+interface LobbyChatLogProps {
+  chatEndRef: React.RefObject<HTMLDivElement>
+}
+
+const LobbyChatLog = ({ chatEndRef }: LobbyChatLogProps) => {
   const { chatLogs } = useChat()
+
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [chatLogs])
+
   return (
     <ul className="max-w-full z-10 text-white px-2 overflow-y-scroll">
       {chatLogs.map((log, index) => (
-        <li key={index} className="flex gap-4">
+        <li
+          key={index}
+          className="flex gap-4 text-white animate-slide-in"
+          style={{ animationDelay: `${index * 0.1}s` }}
+        >
           <span className="shrink-0">{log.nickname} : </span>
           <span className="grow truncate">{log.message}</span>
           <span className="shrink-0">{log.timestamp}</span>
         </li>
       ))}
+      <div ref={chatEndRef} />
     </ul>
   )
 }
