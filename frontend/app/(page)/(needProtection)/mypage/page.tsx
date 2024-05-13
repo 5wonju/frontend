20c36711ea620897f32605cfe325898ae845c699
이우성 <sitpo96@naver.com>
2024-05-13 15:29:39 +0900

    Design #76: 회원정보 페이지 UI 수정

--- frontend/app/(page)/(needProtection)/mypage/page.tsx
+++ frontend/app/(page)/(needProtection)/mypage/page.tsx
@@ -5,7 +5,7 @@ import ModifyNickname from './component/ModifyNickname'
 
 const Mypage = () => {
   return (
-    <section className="">
+    <section className="h-[calc(100vh-3.5rem)] flex flex-col justify-center items-center">
       <UserInfo />
       <ModifyNickname />
     </section>
