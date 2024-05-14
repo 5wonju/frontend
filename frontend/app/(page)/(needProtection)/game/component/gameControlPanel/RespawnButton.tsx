b04328bd00d07bd0e5d250589dcafc27680bdc71
이원주 <dldnjswn4265@gmail.com>
2024-05-14 22:00:19 +0900

    Feat #82: 리스폰 버튼 클릭 핸들러 추가

--- frontend/app/(page)/(needProtection)/game/component/gameControlPanel/RespawnButton.tsx
+++ frontend/app/(page)/(needProtection)/game/component/gameControlPanel/RespawnButton.tsx
@@ -1,8 +1,12 @@
 import { GrPowerCycle } from 'react-icons/gr'
 
 const RespawnButton = () => {
+  const handleRespawn = () => {
+    console.log('중앙 리스폰 얍!')
+  }
+
   return (
-    <div className="icon-btn">
+    <div onClick={handleRespawn} className="icon-btn">
       <GrPowerCycle className="size-8 m-auto text-center" />
     </div>
   )
