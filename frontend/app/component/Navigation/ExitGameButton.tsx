f9959108322b8bc6de3d5db510e186fdf96dc727
이우성 <sitpo96@naver.com>
2024-05-14 13:15:48 +0900

    Feat #87: 게임 중 버튼 비활성화 기능 추가

--- frontend/app/component/Navigation/ExitGameButton.tsx
+++ frontend/app/component/Navigation/ExitGameButton.tsx
@@ -1,15 +1,17 @@
+import { useGameRoomStore } from '@/app/(page)/(needProtection)/game/lib/store'
 import { useGame } from '@/app/hooks/useSocket'
 import React from 'react'
 
 const ExitGameButton = () => {
   const { exitRoom } = useGame()
+  const { gameState } = useGameRoomStore()
 
   const handleExitGame = () => {
     exitRoom()
   }
 
   return (
-    <button onClick={handleExitGame} className="nav-btn">
+    <button onClick={handleExitGame} disabled={gameState !== 'READY'} className="nav-btn">
       게임 나가기
     </button>
   )
