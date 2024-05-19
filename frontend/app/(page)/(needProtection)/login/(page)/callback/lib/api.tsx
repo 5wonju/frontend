fa02482490cc2162c3aefec67f45d2b9d2cb12ee
이우성 <sitpo96@naver.com>
2024-05-20 07:46:16 +0900

    Fix: 배포용 kakako api endpoint로 수정

--- frontend/app/(page)/(needProtection)/login/(page)/callback/lib/api.tsx
+++ frontend/app/(page)/(needProtection)/login/(page)/callback/lib/api.tsx
@@ -3,7 +3,7 @@ import { tokenInstance } from '@/app/axios'
 export const handleLogin = async ({ code, state }: { code: string; state: string }) => {
   try {
     console.log({ code: code, state: state })
-    const response = await tokenInstance.post('/users/oauth/kakao/local-to-distribution', {
+    const response = await tokenInstance.post('/users/oauth/kakao/distribution-to-distribution', {
       code: code,
       state: state,
     })
