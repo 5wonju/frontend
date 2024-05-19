fb360bd5d99ebf2f19630da122a2f084a7354a50
이우성 <sitpo96@naver.com>
2024-05-20 07:13:55 +0900

    Fix: 배포 환경에서 채널 선택을 위해 더미 데이터로 수정

--- frontend/app/(page)/(needProtection)/channel/component/ChannelList.tsx
+++ frontend/app/(page)/(needProtection)/channel/component/ChannelList.tsx
@@ -7,10 +7,18 @@ import { useRouter } from 'next/navigation'
 import { useSocket } from '@/app/hooks/useSocket'
 
 const ChannelList = () => {
-  const { data: channelData } = useQuery({
-    queryKey: ['channel'],
-    queryFn: getChannelData,
-  })
+  // const { data: channelData } = useQuery({
+  //   queryKey: ['channel'],
+  //   queryFn: getChannelData,
+  // })
+  const channelData = [
+    { name: '서울', count: '1000/1000' }, // 포화 (750-1000)
+    { name: '구미', count: '750/1000' }, // 혼잡 (500-749)
+    { name: '대전', count: '500/1000' }, // 보통 (250-499)
+    { name: '광주', count: '250/1000' }, // 쾌적 (0-249)
+    { name: '부산', count: '0/1000' },
+  ]
+  
   const router = useRouter()
   const { socket, isConnected, connectSocket } = useSocket()
   const [selectedChannel, setSelectedChannel] = useState('')
